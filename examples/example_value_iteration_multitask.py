ec991f7cd0620c77d4e29d74edb320f819c7ff26
Ivan Anokhin <ivananokhin@yandex-team.ru>
2022-05-23 22:08:36 +0400

    init

--- examples/example_value_iteration_multitask.py
+++ examples/example_value_iteration_multitask.py
@@ -1,43 +1,35 @@
 import sys
 sys.path.append("..")
 import numpy as np
-from env.grid_world import GridWorld
-from algorithms.dynamic_programming import value_iteration
-from utils.plots import plot_gridworld
+from env.grid_world_multitask import GridWorld
+from algorithms.dynamic_programming_multitask import value_iteration
+from utils.plots_multitask import plot_gridworld
 
 ###########################################################
 #          Run value iteration on a grid world            #
 ###########################################################
 
 # specify world parameters
-# num_cols = 11
-# num_rows = 11
-# obstructions = np.array([[5,0],[5,1],[5,2],[5,3],[5,4],
-#                          [5,6],[5,7],[5,8],[5,9],[5,10]])
-# # bad_states = np.array([[1,9],[4,2],[4,4],[7,5],[9,9]])
-# # restart_states = np.array([[3,7],[8,2]])
-# start_state = np.array([[0,4]])
-# # start_state = np.array([[0,0], [0,1], [0,2], [0,3], [0,4], [0,5], [0,6],
-# #                         [0,7], [0,8],[0,9], [0,10]])
-# # goal_states = start_state + np.array([[10, 0]])
-# goal_states = np.array([[9,9]])
-
 num_cols = 3
 num_rows = 3
 obstructions = np.array([[1,0],[1,2]])
-start_state = np.array([[0,0]])
-start_states_plot = np.array([[0,0], [0,2]])
+start_state = np.array([[0,0], [0,2]])
+start_states_plot = start_state
+# start_states_plot = np.array([[0,0], [0,2]])
 # goal_states_plot = np.array([[2,2], [2, 2]])
 # start_states_plot = np.array([[0,0]], )
-goal_states_plot = np.array([[2,2]], )
-goal_states = np.array([[2,2]])
+# goal_states_plot = np.array([[2,2]])
+# goal_states = np.array([[2, 0], [2, 1]])
+goal_states = np.array([[0,0], [0,1], [0,2], [1, 1], [2, 1], [2,2], [2, 0]])
+goal_states_plot = goal_states
 
 # create model
 gw = GridWorld(num_rows=num_rows,
                num_cols=num_cols,
                start_state=start_state,
                goal_states=goal_states)
-gw.add_obstructions(obstructed_states=obstructions,
+gw.add_obstructions(
+                    obstructed_states=obstructions,
                     # bad_states=bad_states,
                     # restart_states=restart_states
                     )
@@ -51,8 +43,14 @@ gw.add_transition_probability(p_good_transition=1.,
 gw.add_discount(discount=0.9)
 model = gw.create_gridworld()
 
+# print('model created')
+
 # solve with value iteration
 value_function, policy = value_iteration(model, maxiter=100)
+
+# print('policy created')
+
+# print('policy', policy.shape)
 
 # plot the results
 path = "../doc/imgs/value_iteration.png"
@@ -60,3 +58,5 @@ plot_gridworld(model, value_function=value_function, policy=policy,
                # title="Value iteration",
                path=path,
                start_states=start_states_plot, goal_states=goal_states_plot)
+
+# print('plot saved')
